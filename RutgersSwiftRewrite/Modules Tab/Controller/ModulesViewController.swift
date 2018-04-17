0ec1723275a990b4c99759951f6aae7bbfdb2e19
Salman Fakhri <salmanfakhri2@gmail.com>
2018-04-17 17:14:32 -0400

    file restructure

---
+++ RutgersSwiftRewrite/Modules Tab/Controller/ModulesViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ModulesViewController.swift
+//  RutgersSwiftRewrite
+//
+//  Created by Salman Fakhri on 4/17/18.
+//  Copyright © 2018 Colin Walsh. All rights reserved.
+//
+
+import UIKit
+
+class ModulesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
